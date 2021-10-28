fcbd990c83637785f852095cb1eab636db45ce46
Elson Correia <13211046+ECorreia45@users.noreply.github.com>
2021-10-27 22:58:33 -0400

    "use strict"

--- src/utils/evaluate-string-in-component-context.ts
+++ src/utils/evaluate-string-in-component-context.ts
@@ -1,7 +1,7 @@
 export function evaluateStringInComponentContext(executableString: string, src: WebComponent, propertyNames: Array<string> = []) {
 	const values = propertyNames.map(key => src[key]);
 
-	const fn = new Function(...propertyNames, `return ${executableString}`);
+	const fn = new Function(...propertyNames, `"use strict";return ${executableString}`);
 
 	return fn.apply(src, values) ?? '';
 }
