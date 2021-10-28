3d156cab6fbf68dffe1f2a3dc9fecef0deb601af
Elson Correia <design@elsoncorreia.com>
2021-10-28 19:36:30 -0400

    update parser to handle composed hashed attributes

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -63,16 +63,24 @@ function setAttributes(node: HTMLElement, attributes: string) {
 	let match: RegExpExecArray | null = null;
 
 	while ((match = attrPattern.exec(attributes))) {
-		const name = match[1];
+		let name = match[1];
 		const value = match[2] || match[3] || match[4] || (
 			new RegExp(`^${match[1]}\\s*=`).test(match[0]) ? '' : null
 		)
 
 		if (name.startsWith('#')) {
+			const dot = name.indexOf('.');
+			let prop = null;
+
+			if (dot >= 0) {
+				prop = name.slice(dot + 1);
+				name = name.slice(0, dot);
+			}
+
 			// this is a special handler for custom attributes since its syntax is not allowed
 			// for HTML attributes allowing this parser to work with normal DOM elements
 			// @ts-ignore
-			node[name] = value;
+			node[name] = {value, prop};
 		} else {
 			node.setAttribute(name, value ?? '');
 		}
