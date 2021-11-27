10a5f4325454f8921378c3bb3645b0c547bf5cb1
Elson Correia <design@elsoncorreia.com>
2021-11-27 08:40:30 -0500

    utils tests

--- src/utils/resolve-executable.ts
+++ src/utils/resolve-executable.ts
@@ -3,7 +3,6 @@ import {evaluateStringInComponentContext} from "./evaluate-string-in-component-c
 export function resolveExecutable(component: WebComponent, nodeData: ObjectLiteral, {match, executable}: Executable, newValue: string) {
 	let res = evaluateStringInComponentContext(executable, component, nodeData);
 
-
 	if (res && typeof res === 'object') {
 		try {
 			res = JSON.stringify(res)
