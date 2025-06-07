4d9182733e2c9e424c233b52188bdfd68ac97f3f
Elson Correia <elcorreia45@gmail.com>
2025-06-07 18:26:12 -0400

    simplify turnCamelToKebabCasing

--- src/utils/turn-camel-to-kebab-casing.ts
+++ src/utils/turn-camel-to-kebab-casing.ts
@@ -1,13 +1,6 @@
-export const turnCamelToKebabCasing = (name: string) => {
-    const match = name.match(/(?:[A-Z]+(?=[A-Z][a-z])|[A-Z]+|[a-zA-Z])[^A-Z]*/g)
-
-    if (match) {
-        for (let i = 0; i < match.length; i++) {
-            match[i] = match[i].toLowerCase()
-        }
-
-        return match.join('-')
-    }
-
-    return name
+export function turnCamelToKebabCasing(str: string): string {
+    return str
+        .replace(/([a-z0-9])([A-Z])/g, '$1-$2') // lower/number followed by upper
+        .replace(/([A-Z]+)([A-Z][a-z])/g, '$1-$2') // acronym followed by capitalized word
+        .toLowerCase()
 }
